b586f5d919386c8b6016e9d58994c0afb1f788ff
Tran Duy Ngoc <Tranduyngoc1009@gmail.com>
2022-05-09 15:26:56 +0700

    the first commit

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void Load(int index)
+    {
+        SceneManager.LoadScene(index); 
+        // nap scene level 1 bbbb
+    }
+
+}
